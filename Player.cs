554585def32da1927b3fdd065178279f20d87f44
Yerdun <56048350+Yerdun@users.noreply.github.com>
2021-04-06 15:38:22 -0700

    Implemented basic focus ability FocusHitbox: Temporary graphic for hitbox representation. Comes up when you focus ingame Player: Added focus capabilities to the script, relying on bool

--- Player.cs
+++ Player.cs
@@ -7,8 +7,9 @@ public class Player : Area2D
 	// private int a = 2;
 	// private string b = "text";
 	[Export]
-	public float speed = 400f;
-	public float focusMultiplier = 0.5f;
+	public int unfocusedSpeed = 200;
+	[Export]
+	public int focusSpeed = 100;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,26 +22,27 @@ public class Player : Area2D
 	{
 		var velocity = new Vector2();
 		bool isFocused = false;
-		
-		if (Input.IsActionPressed("control_focus"))
-			isFocused = true;
+		int speed = unfocusedSpeed;
 		
 		if (Input.IsActionPressed("ui_right"))
 			velocity.x += 1;
-		
 		if (Input.IsActionPressed("ui_left"))
 			velocity.x -= 1;
-		
 		if (Input.IsActionPressed("ui_up"))
 			velocity.y -= 1;
-		
 		if (Input.IsActionPressed("ui_down"))
 			velocity.y += 1;
+		if (Input.IsActionPressed("control_focus"))
+			isFocused = true;
+		if (Input.IsActionJustReleased("control_focus"))
+			isFocused = false;
 		
-		if (velocity.Length() > 0 && isFocused)
-			velocity = velocity.Normalized() * speed * focusMultiplier;
+		if (isFocused)
+			speed = focusSpeed;
+		else if (isFocused == false)
+			speed = unfocusedSpeed;
 		
-		else if (velocity.Length() > 0 && !isFocused)
+		if (velocity.Length() > 0)
 			velocity = velocity.Normalized() * speed;
 		
 		Position += velocity * delta;
