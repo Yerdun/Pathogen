0f66e1828162b2ef012d923e3d72a6550137aaf1
Yerdun <56048350+Yerdun@users.noreply.github.com>
2021-05-11 02:42:08 -0700

    Started Piercer enemy

--- Assets/Scenes/Enemy/Shooter Enemy/Shooter Enemy.gd
+++ Assets/Scenes/Enemy/Shooter Enemy/Shooter Enemy.gd
@@ -13,4 +13,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Enemy starts out moving, opposite force slows it to a halt
+	if linear_velocity.x >= 0:	# When the force pushes it enough to move right
+		applied_force = Vector2.ZERO	# Stop the enemy (no force, no velocity)
+		linear_velocity.x = 0
+		
+#		var look_vec = get_global_mouse_position() - global_position
+#		global_rotation = atan2(look_vec.y, look_vec.x)
+#		Pseudocode stub:
+#			Get direction of player at the current frame (may need to make its own function)
+#			Turn sprite in said direction (take half a second to do so)
+#			Instantiate bullet
+#			Turn it in said direction
+#			Set linear velocity of bullet to 400 (This has the effect of shooting bullets aimed at the player)
+#			Do this until ten bullets are fired
+#			Wait one second
+#			Move and rotate the enemy in the direction of the player
+#			Set speed to -500 (normalised in player direction) and force to 450
+#			(Then the process should start over)
+			
