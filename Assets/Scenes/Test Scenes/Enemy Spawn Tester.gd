6e19b53b3cb10750eb2dc7a848ffd2562c5e7de0
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-05-11 01:27:06 -0700

    Enemies no longer collide with each other; Added enemy spawner; created Enemy Spawn Tester to demonstrate
    Enemies' collision layers have all been changed to 2 so that they no longer collide with each other. Added the Enemy Spawn Handler scene, which can spawn in enemies at one of seven spawn points. Created the Enemy Spawn Tester to demonstrate how the enemy spawn handler can work - move the slider to select the spawnpoint and use the buttons to spawn different types of enemies.

---
+++ Assets/Scenes/Test Scenes/Enemy Spawn Tester.gd
@@ -0,0 +1,31 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+var SelectedSpawnpoint = 1
+
+export (PackedScene) var bombEnemy
+export (PackedScene) var piercerEnemy
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Selector_value_changed(value):
+	SelectedSpawnpoint = int(value)
+	$Label.text = str(value)
+
+
+func _on_Spawn_Piercer_pressed():
+	$"Enemy Spawn Handler".spawnEnemy(piercerEnemy, SelectedSpawnpoint)
+
+
+func _on_Spawn_Bomb_pressed():
+	$"Enemy Spawn Handler".spawnEnemy(bombEnemy, SelectedSpawnpoint)
