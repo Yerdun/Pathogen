21a313ef78bfa741b9d75bfea787da911eaaf847
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-05-10 18:03:40 -0700

    Added a new enemy to the copy tester that can die
    Added a new enemy to the Copy Tester scene. When hit twice with a basic bullet, it dies, and adds charge to the charge bar for the copy ability. The code on the dummies could be re-used for basic enemies.

--- Scenes/Bullet/Piercing Bullet/Piercing Bullet.gd
+++ Scenes/Bullet/Piercing Bullet/Piercing Bullet.gd
@@ -1,10 +1,24 @@
 extends Area2D
 
-
 export var speed = 900 # the speed at which the bullet will travel
+export var damage = 1 # the amount of damage the bullet does to enemies on hit
+
+signal killed_enemy
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	position += Vector2.RIGHT * speed * delta # go right
 
-func _on_Bullet_body_entered(_body):
-	pass
+
+func _on_Bullet_body_entered(body):
+	if body.has_method("takeDamage"): # if the hit body can take damage
+		body.takeDamage(damage) # do damage to it equal to this bullet's damage value
+		if body.currentHP <= 0: # if you killed the enemy,
+			_killed_enemy() # neat! tell everyone, including the player
+	
+	# the bullet pierces, so it doesn't destroy itself
+
+func _killed_enemy():
+	emit_signal("killed_enemy")
