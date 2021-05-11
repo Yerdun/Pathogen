117f6a1942291653127d150359e8875f914e1023
Yerdun <56048350+Yerdun@users.noreply.github.com>
2021-05-10 18:12:09 -0700

    Fixed file heirarchy and player script

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
 extends Area2D
 
 export var standard_speed = 300	# Standard speed
@@ -36,7 +31,6 @@ func _process(delta):
 	elif is_focused == false:
 		speed = standard_speed
 		$FocusAnimation.play("default") # Placeholder blank animation
-<<<<<<< HEAD
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -45,63 +39,3 @@ func _process(delta):
 
 func _on_Player_body_entered(body):
 	print("Object in player body") # Stub console output for hit detection
-=======
-extends KinematicBody2D
-=======
-extends Area2D
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
-
-export var standard_speed = 300	# Standard speed
-export var focus_speed = 140	# Speed while focused (slow mode)
-
-func _ready():
-	pass # Currently unneeded, included as a stub.
-
-func _process(delta):
-	var velocity = Vector2.ZERO # Movement velocity represented as a 2 dimensional vector
-	var speed = standard_speed # Speed variable. initialised to standard speed
-	var is_focused = false # Tracks whether or not player is focused
-	#var player_animation = $PlayerAnimation # Method used to play and stop player animation
-	#var focus_animation = $FocusAnimation # Method used to play and stop focusing animation
-
-	# Controls: Arrow keys to move, Shift to focus (can change in Project Settings > Input Map)
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("control_focus"):
-		is_focused = true
-	
-	if is_focused == true:
-		speed = focus_speed
-		$FocusAnimation.play("focused") # Placeholder animation just containing 1 frame showing hitbox
-	elif is_focused == false:
-		speed = standard_speed
-		$FocusAnimation.play("default") # Placeholder blank animation
-=======
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-func kill():
-	get_tree().reload_current_scene()		
-	
->>>>>>> 73856d9 (Switched Player script from C# to GDScript)
-=======
-=======
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
-	position += velocity * delta
-
-func _on_Player_body_entered(body):
-	print("Object in player body") # Stub console output for hit detection
-<<<<<<< HEAD
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
-=======
->>>>>>> 15dd5e0 (Fixed Player.gd, moved Hitbox Test scene)
