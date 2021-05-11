dd6629f87c12295077b1fb418672e49d140094d6
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-05-10 21:37:22 -0700

    Implemented some of the sprites; began work on the bomb enemy
    Changed several scenes to use the game's sprites instead of the Godot logo. Began implementing the bomb enemy, and created the Bomb Tester scene to demonstrate its explosion bullet pattern.

--- Assets/Scenes/Enemy/Basic Enemy/Enemy.gd
+++ Assets/Scenes/Enemy/Basic Enemy/Enemy.gd
@@ -1,7 +1,5 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
 export var maxHP = 2 # the amount of hp the enemy has at spawn
 var currentHP # the enemy's current hp
 export(String, "none", "rapidfire", "widebeam", "piercing") var copyAbility	# The enemy's ability
