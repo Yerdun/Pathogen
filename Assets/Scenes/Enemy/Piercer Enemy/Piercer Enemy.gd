cdf098d5fd28e2f5bfb0543a35c078c555f21faa
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-05-12 18:40:24 -0700

    Revert "Merge branch 'demo' into Miguel-branch"
    This reverts commit 8453a1a3d963b1f79d218a674ad33e758e5544b2, reversing
    changes made to 016cdf2b4f5d622dfef50cdde34cfa2a3fe2388d.

--- Assets/Scenes/Enemy/Piercer Enemy/Piercer Enemy.gd
+++ Assets/Scenes/Enemy/Piercer Enemy/Piercer Enemy.gd
@@ -8,7 +8,7 @@ var _waitingToCharge = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"Hitbox".shape.extents = Vector2(32,24)	# Hitbox better fits sprite
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Assets/Scenes/Player/Player.gd
+++ Assets/Scenes/Player/Player.gd
@@ -10,7 +10,7 @@ func _ready():
 	pass # Currently unneeded, included as a stub.
 
 func _process(delta):
-	if !is_dead:	# Disallow movement when dead
+	if is_dead == false:	# Disallow movement when dead
 		var velocity = Vector2.ZERO # Movement velocity represented as a 2 dimensional vector
 		var speed = standard_speed # Speed variable. initialised to standard speed
 		var is_focused = false # Tracks whether or not player is focused
@@ -27,10 +27,10 @@ func _process(delta):
 		if Input.is_action_pressed("control_focus"):
 			is_focused = true
 		
-		if is_focused:
+		if is_focused == true:
 			speed = focus_speed
 			$FocusAnimation.play("focused") # Placeholder animation just containing 1 frame showing hitbox
-		elif !is_focused:
+		elif is_focused == false:
 			speed = standard_speed
 			$FocusAnimation.play("default") # Placeholder blank animation
 			
@@ -50,16 +50,13 @@ func _on_Player_body_entered(body):
 		$"Bullet Control".CurrentCharge = $"Bullet Control".MaxCharge	# Max copy charge if colliding with copy charger
 		body.queue_free()	# Remove body
 	
-	elif !is_dead:	# If colliding with anything else eg. enemy, bullet, die (don't allow dying while respawning)
+	else:	# If colliding with anything else (enemy, bullet)
 		lives -= 1	# Subtract one life
 		$PlayerAnimation.play("death")	# Play death animation (placeholder blank)
 		is_dead = true	# Set is_dead variable to true, to lock control
 		yield(get_tree().create_timer(0.5), "timeout")	# Wait half a second
-		if lives >= 1:	# When player still has lives
-			position.x = 160	# Reset player position (temporarily the Player node position defaults, change later)
-			position.y = 120
-			$PlayerAnimation.play("default")	# Return to default animation
-			is_dead = false	# Change is_dead to false to regain control
-		else:	# Game over routine
-			get_tree().change_scene("res://Assets/Scenes/Test Scenes/Game Over.tscn")
+		position.x = 160	# Reset player position (temporarily the Player node position defaults, change later)
+		position.y = 120
+		$PlayerAnimation.play("default")	# Return to default animation
+		is_dead = false	# Change is_dead to false to regain control
 	
