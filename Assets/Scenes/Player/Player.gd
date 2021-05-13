a4473b0498ed433b50d6f463365a58ae354117b9
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-05-12 18:41:39 -0700

    Revert "Revert "Merge branch 'demo' into Miguel-branch""
    This reverts commit cdf098d5fd28e2f5bfb0543a35c078c555f21faa.

--- Assets/Scenes/Player/Player.gd
+++ Assets/Scenes/Player/Player.gd
@@ -10,7 +10,7 @@ func _ready():
 	pass # Currently unneeded, included as a stub.
 
 func _process(delta):
-	if is_dead == false:	# Disallow movement when dead
+	if !is_dead:	# Disallow movement when dead
 		var velocity = Vector2.ZERO # Movement velocity represented as a 2 dimensional vector
 		var speed = standard_speed # Speed variable. initialised to standard speed
 		var is_focused = false # Tracks whether or not player is focused
@@ -27,10 +27,10 @@ func _process(delta):
 		if Input.is_action_pressed("control_focus"):
 			is_focused = true
 		
-		if is_focused == true:
+		if is_focused:
 			speed = focus_speed
 			$FocusAnimation.play("focused") # Placeholder animation just containing 1 frame showing hitbox
-		elif is_focused == false:
+		elif !is_focused:
 			speed = standard_speed
 			$FocusAnimation.play("default") # Placeholder blank animation
 			
@@ -50,13 +50,16 @@ func _on_Player_body_entered(body):
 		$"Bullet Control".CurrentCharge = $"Bullet Control".MaxCharge	# Max copy charge if colliding with copy charger
 		body.queue_free()	# Remove body
 	
-	else:	# If colliding with anything else (enemy, bullet)
+	elif !is_dead:	# If colliding with anything else eg. enemy, bullet, die (don't allow dying while respawning)
 		lives -= 1	# Subtract one life
 		$PlayerAnimation.play("death")	# Play death animation (placeholder blank)
 		is_dead = true	# Set is_dead variable to true, to lock control
 		yield(get_tree().create_timer(0.5), "timeout")	# Wait half a second
-		position.x = 160	# Reset player position (temporarily the Player node position defaults, change later)
-		position.y = 120
-		$PlayerAnimation.play("default")	# Return to default animation
-		is_dead = false	# Change is_dead to false to regain control
+		if lives >= 1:	# When player still has lives
+			position.x = 160	# Reset player position (temporarily the Player node position defaults, change later)
+			position.y = 120
+			$PlayerAnimation.play("default")	# Return to default animation
+			is_dead = false	# Change is_dead to false to regain control
+		else:	# Game over routine
+			get_tree().change_scene("res://Assets/Scenes/Test Scenes/Game Over.tscn")
 	
